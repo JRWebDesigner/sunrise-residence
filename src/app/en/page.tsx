6022d4e584cc6b02e24cbb197eda87f0c515cdac
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-05-15 07:06:16 -0400

    Update page.tsx

--- src/app/en/page.tsx
+++ src/app/en/page.tsx
@@ -69,7 +69,7 @@ export default function HomeEn() {
               <div className="pl-4 mt-2 text-base">
                 <Link href="/es" onClick={() => setOpen(false)}>Español</Link><br />
                 <Link href="/en" onClick={() => setOpen(false)}>English</Link><br />
-                <Link href="/" onClick={() => setOpen(false)}>Italiano</Link>
+                <Link href="/" onClick={() => setOpen(false)}>Italiano</Link><br />
                 <Link href="/fr" onClick={() => setOpen(false)}>Frances</Link>
               </div>
             </div>
